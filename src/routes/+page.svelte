a6d30a4a9d7de87ddd9418bf0efa233602d81440
cattodotpy <harrowfung1016@gmail.com>
2025-01-01 17:46:05 +0800

    added link to countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,79 @@
 <div class="flex flex-col">
-    <div class="flex items-center h-62 mt-10 ml-24 flex-row gap-5">
-        <div class="flex items-center">
-            <div id="avatar-mask" class="size-56 rounded-full overflow-hidden">
-                <img src="avatar.png" alt="Avatar" />
-            </div>
-        
-            <div class="ml-8 flex flex-col gap-2">
-                <h1 class="text-4xl font-bold">Harrow Fung </h1>
-                <span class="text-md">(also known as catto)</span>
-                <p class="text-md text-gray-400">Just an ordinary secondary student codes as interest.</p>
-            </div>  
-        </div>
-    
-        <!-- seperator -->
-        <div class="border-r border-gray-500 h-24 ml-8"></div>  
-    
-        <div>
-            what the hell is this
-            <br />
-            why am I doing this
-            <br />
-            coding is bad
-            <br />
-            im a backend dev, not a frontend dev. im not suppose to make this good
-            
-        </div>
-    </div>
+	<div class="h-62 ml-24 mt-10 flex flex-row items-center gap-5">
+		<div class="flex items-center">
+			<div id="avatar-mask" class="size-56 overflow-hidden rounded-full">
+				<img src="avatar.png" alt="Avatar" />
+			</div>
 
-    <div class="border-b border-gray-500 h-24 mx-16"></div>  
+			<div class="ml-8 flex flex-col gap-2">
+				<h1 class="text-4xl font-bold">Harrow Fung</h1>
+				<span class="text-md">(also known as catto)</span>
+				<p class="text-md text-gray-400">Just an ordinary secondary student codes as interest.</p>
+			</div>
+		</div>
 
-    <div class="flex flex-col gap-5 left-24 relative">
-        <div class="mt-3"></div>
-        <h1 class="text-4xl font-bold">Projects </h1>
-        <p class="text-1xl">(all of them are half done and forgotten for months)</p>
-        <div class="flex flex-row gap-10  mt-10">
-            <div class="flex flex-col gap-5">
-                <h2 class="text-2xl font-bold">404 not found.</h2>
-                <p class="text-md text-gray-400">what</p>
-            </div>
-            <div class="flex flex-col gap-5">
-                <h2 class="text-2xl font-bold">404 not found.</h2>
-                <p class="text-md text-gray-400">the</p>
-            </div>
-            <div class="flex flex-col gap-5">
-                <h2 class="text-2xl font-bold">404 not found.</h2>
-                <p class="text-md text-gray-400"> fuck</p>
-            </div>
-        </div>
-    </div>
+		<!-- seperator -->
+		<div class="ml-8 h-24 border-r border-gray-500"></div>
 
-    <div class="border-b border-gray-500 h-24 mx-16"></div>  
+		<div>
+			what the hell is this
+			<br />
+			why am I doing this
+			<br />
+			coding is bad
+			<br />
+			im a backend dev, not a frontend dev. im not suppose to make this good
+		</div>
+	</div>
 
-    <div class="left-24 relative flex-col">
-        <!-- contacts -->
-        <div class="mt-6"></div>
-        <h1 class="text-4xl font-bold">Links</h1>
-        <div class="flex flex-row mt-5 gap-4">
-            <a href="https://github.com/cattodotpy">
-                <div class="p-3 bg-slate-500 text-2xl font-bold rounded-xl flex flex-row gap-3">
-                    <div class="size-8">
-                        <img src="github-mark.png" alt="github" />
-                    </div>
-                    GitHub
-                </div>
-            </a>
+	<div class="mx-16 h-24 border-b border-gray-500"></div>
 
-            <a href="https://discord.com/users/caatto">
-                <div class="p-3 bg-zinc-500 text-2xl font-bold rounded-xl flex flex-row gap-3">
-                    <div class="size-8">
-                        <img src="discord-mark-blue.png" alt="discord" />
-                    </div>
-                    Discord
-                </div>
-            </a>
-        </div>  
-    </div>
-    
+	<div class="relative left-24 flex flex-col gap-5">
+		<div class="mt-3"></div>
+		<h1 class="text-4xl font-bold">Projects</h1>
+		<p class="text-1xl">(all of them are half done and forgotten for months)</p>
+		<div class="mt-10 flex flex-row gap-10">
+			<div class="flex flex-col gap-5">
+				<h2 class="text-2xl font-bold">Countdown page</h2>
+				<p class="text-md text-gray-400">
+					<a href="/countdown" class="text-blue-300 hover:underline">link</a>
+				</p>
+			</div>
+			<div class="flex flex-col gap-5">
+				<h2 class="text-2xl font-bold">404 not found.</h2>
+				<p class="text-md text-gray-400">the</p>
+			</div>
+			<div class="flex flex-col gap-5">
+				<h2 class="text-2xl font-bold">404 not found.</h2>
+				<p class="text-md text-gray-400">fuck</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="mx-16 h-24 border-b border-gray-500"></div>
+
+	<div class="relative left-24 flex-col">
+		<!-- contacts -->
+		<div class="mt-6"></div>
+		<h1 class="text-4xl font-bold">Links</h1>
+		<div class="mt-5 flex flex-row gap-4">
+			<a href="https://github.com/cattodotpy">
+				<div class="flex flex-row gap-3 rounded-xl bg-slate-500 p-3 text-2xl font-bold">
+					<div class="size-8">
+						<img src="github-mark.png" alt="github" />
+					</div>
+					GitHub
+				</div>
+			</a>
+
+			<a href="https://discord.com/users/caatto">
+				<div class="flex flex-row gap-3 rounded-xl bg-zinc-500 p-3 text-2xl font-bold">
+					<div class="size-8">
+						<img src="discord-mark-blue.png" alt="discord" />
+					</div>
+					Discord
+				</div>
+			</a>
+		</div>
+	</div>
 </div>
